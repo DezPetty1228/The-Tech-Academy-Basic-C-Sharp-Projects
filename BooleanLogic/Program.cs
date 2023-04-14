7d043e3d028aac42b85bc126d24eaa643a611db9
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-04-14 14:02:23 -0700

    Insurance Qualification Assignment
    Your job is to create a basic approval program for car insurance. It will ask the applicant some questions and return a “true” or “false” at the end to determine whether or not the applicant qualified for car insurance.

--- BooleanLogic/Program.cs
+++ BooleanLogic/Program.cs
@@ -6,29 +6,18 @@ namespace BooleanLogic
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(true && true && true || false);
-            Console.WriteLine(true && false);
-            Console.WriteLine(true && true);
-            Console.WriteLine(false && false);
-
-            Console.WriteLine(true && true && true || false);
-            Console.WriteLine(true || false);
-            Console.WriteLine(true || true);
-            Console.WriteLine(false || false);
-
-            Console.WriteLine(true == true && true || false);
-            Console.WriteLine(true == false);
-            Console.WriteLine(true == true);
-            Console.WriteLine(false == false);
-
-            Console.WriteLine(true != true && true || false);
-            Console.WriteLine(true != false);
-            Console.WriteLine(true != true);
-            Console.WriteLine(false != false);
-
-            Console.WriteLine(true ^ false);
-            Console.WriteLine(true ^ true);
-            Console.WriteLine(false ^ false);
+            Console.WriteLine("What is your age ?");
+            string age = Console.ReadLine();
+            int old = Convert.ToInt32(age);
+            Console.WriteLine("Have you ever had a DUI? please answer true or false");
+            string dui = Console.ReadLine();
+            bool hasDUI = Convert.ToBoolean(dui);
+            Console.WriteLine("How many speeding tickets do you have ?");
+            string speed = Console.ReadLine();
+            int tickets = Convert.ToInt32(speed);
+            Console.WriteLine("Qualified ?");
+            bool isQualified = old >= 16 && (hasDUI != true && tickets <= 1);
+            Console.WriteLine(isQualified);
 
         }
     }
