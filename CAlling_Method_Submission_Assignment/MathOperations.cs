11a0e10cd45922f0cd9222b37b963d77e8d27549
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-04-20 20:55:58 -0700

    CAlling_Method_Submission_Assignment pt 2

--- CAlling_Method_Submission_Assignment/MathOperations.cs
+++ CAlling_Method_Submission_Assignment/MathOperations.cs
@@ -6,5 +6,22 @@ namespace CAlling_Method_Submission_Assignment
 {
     class MathOperations
     {
+        public static int AddTwo(int number)
+        {
+            // Adds 2 to the input number and returns the result.
+            return number + 2;
+        }
+
+        public static int MultiplyByFive(int number)
+        {
+            // Multiplies the input number by 5 and returns the result.
+            return number * 5;
+        }
+
+        public static int Square(int number)
+        {
+            // Squares the input number and returns the result.
+            return number * number;
+        }
     }
 }
--- CAlling_Method_Submission_Assignment/Program.cs
+++ CAlling_Method_Submission_Assignment/Program.cs
@@ -4,9 +4,24 @@ namespace CAlling_Method_Submission_Assignment
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            // Ask the user what number they want to perform math operations on.
+            Console.Write("Enter a number: ");
+            int number = int.Parse(Console.ReadLine());
+
+            // Call each of the MathOperations methods with the user's input and display the result to the screen.
+            int result1 = MathOperations.AddTwo(number);
+            Console.WriteLine($"Adding 2: {result1}");
+
+            int result2 = MathOperations.MultiplyByFive(number);
+            Console.WriteLine($"Multiplying by 5: {result2}");
+
+            int result3 = MathOperations.Square(number);
+            Console.WriteLine($"Squaring: {result3}");
+
+            // Wait for user input before closing the console window.
+            Console.ReadLine();
         }
     }
 }
