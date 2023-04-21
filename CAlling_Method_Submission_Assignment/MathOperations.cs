00a99ae8616ad16e7306afb7df5397ea3a944c2e
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-04-20 20:54:12 -0700

    CAlling_Method_Submission_Assignment

---
+++ CAlling_Method_Submission_Assignment/MathOperations.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CAlling_Method_Submission_Assignment
+{
+    class MathOperations
+    {
+    }
+}
---
+++ CAlling_Method_Submission_Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CAlling_Method_Submission_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
