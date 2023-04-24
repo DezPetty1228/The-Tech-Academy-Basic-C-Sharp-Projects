052e3b8fe1393aae27681b204c2b5b918020272f
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-04-24 16:52:51 -0700

    Changed names to mitigate confusion

--- Twentyone/TwentyOneGame.cs
+++ Twentyone/TwentyOneGame.cs
@@ -4,11 +4,12 @@ using System.Text;
 
 namespace Twentyone
 {
-    public class TwentyOneGame : Game
+    // TwentyOneGame is able to inherit methods from : Game Classs
+    public class TwentyOneGame : Game // : allows new classes to inherit methods from other classes. 
     {
-       public void Play()
+       public void Play() // created a method callled play that will be specific to TwentyOneGame
         {
-            throw new NotImplementedException();
+            throw new NotImplementedException(); // thows error if Play method is used without being defined first
         }
 
     }
