46c081bec70e50f9d8f35ae5e0be05ba41f9e4bf
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-04-24 18:35:22 -0700

    pg 281 Method Class assignment

---
+++ Step281MethodClass/MathTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Step281MethodClass
+{
+    public class MathTest
+    {
+        // Create a class. In that class, create a void method that takes two integers as parameters.
+        public void DivisionMath(int numerator, int denominator)
+        {
+            //  Have the method do a math operation on the first integer and display the second integer to the screen.
+            // divide 2 numbers
+            int result = numerator / 2;
+            Console.WriteLine(denominator);
+        }
+    }
+}
+
+    
+
---
+++ Step281MethodClass/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Step281MethodClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            MathTest math = new MathTest(); // In the Main() method of the console app, instantiate the class.
+
+            // Call the method in the class, passing in two numbers.
+            math.DivisionMath(numerator: 12, denominator: 2); // Call the method in the class, specifying the parameters by name
+            Console.WriteLine();
+
+            math.DivisionMath(25, 5);
+            Console.ReadLine();
+        }
+    }
+}
