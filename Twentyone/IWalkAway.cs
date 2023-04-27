0442cf54696cade4f02821af749d54b232eb31b7
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-04-26 19:55:00 -0700

    polymorphism submission

---
+++ Twentyone/IWalkAway.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Twentyone
+{
+    interface IWalkAway
+    {
+         void WalkAway(Player player);
+    }
+}
--- Twentyone/TwentyOneGame.cs
+++ Twentyone/TwentyOneGame.cs
@@ -5,12 +5,20 @@ using System.Text;
 namespace Twentyone
 {
     // TwentyOneGame is able to inherit methods from : Game Classs
-    public class TwentyOneGame : Game // : allows new classes to inherit methods from other classes. 
+    public class TwentyOneGame : Game, IWalkAway // : allows new classes to inherit methods from other classes. 
     {
-       public void Play() // created a method callled play that will be specific to TwentyOneGame
+       public override void Play() // created a method callled play that will be specific to TwentyOneGame
         {
             throw new NotImplementedException(); // thows error if Play method is used without being defined first
         }
-
+        public override void ListPlayers()
+        {
+            Console.WriteLine("21 Players: " );
+            base.ListPlayers();
+        }
+        public void WalkAway(Player player)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
