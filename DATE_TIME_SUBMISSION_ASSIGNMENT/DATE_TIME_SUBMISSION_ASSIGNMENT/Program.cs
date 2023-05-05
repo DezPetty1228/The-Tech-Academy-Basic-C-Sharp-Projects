af514cf62c5087cc30134920c774dbba7c7fa970
DezPetty1228 <117389373+DezPetty1228@users.noreply.github.com>
2023-05-05 12:26:41 -0700

    Debugged Assignment with FRank

--- DATE_TIME_SUBMISSION_ASSIGNMENT/DATE_TIME_SUBMISSION_ASSIGNMENT/Program.cs
+++ DATE_TIME_SUBMISSION_ASSIGNMENT/DATE_TIME_SUBMISSION_ASSIGNMENT/Program.cs
@@ -10,8 +10,8 @@ namespace DATE_TIME_SUBMISSION_ASSIGNMENT
             Console.WriteLine("Current Date and Time: " + currentDateTime); // displays current time
             Console.WriteLine(" please enter a number between 1 and 20 ");   // ask for user to imput a number
             int hoursAhead = Convert.ToInt32(Console.ReadLine()); // converts user imput to int and assigns it to hoursAhead
-            Console.WriteLine("in {0} hours the time will be: ", (hoursAhead) + (currentDateTime.AddHours(hoursAhead));
-
+            Console.WriteLine("in {0} hours the time will be: {1}", hoursAhead, currentDateTime.AddHours(hoursAhead));
+            Console.WriteLine("in " + hoursAhead + " hours It will be " + currentDateTime.AddHours(hoursAhead));
         }
     }
 }
